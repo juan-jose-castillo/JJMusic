f2852b44cb9f6af3e62472a4b80d5dc658c88f36
castillocode <jjandroid2015@gmail.com>
2017-09-18 20:17:30 -0400

    OK

--- app/src/main/java/com/castillo/android/jjmusic/Services/ArtistDatabaseService.java
+++ app/src/main/java/com/castillo/android/jjmusic/Services/ArtistDatabaseService.java
@@ -16,28 +16,14 @@ import retrofit2.http.Query;
  */
 
 public interface ArtistDatabaseService {
-    //https://api.themoviedb.org/3/movie/now_playing?language=es&api_key=589e
-//    @GET("movie/now_playing?language=es")
-//    Call<PeliculasCineRespuesta> obtenerPeliculasEnCines(@Query("api_key") String apiKey);
-//lo que falta de la url
-// geo.gettopartists&country=bolivia&api_key=123d7d50ff
+
     @GET("?method=geo.gettopartists&country=bolivia")
     Call<TopArtist> obtenerArtistas(@Query("api_key") String apikey,
                                     @Query("format")String format,@Query("page")int page);
-//@GET("?method=geo.gettopartists&country=bolivia")
-//    Call<ArtistasRespuesta> obtenerArtistas(@Query("api_key") String apikey,
-//
-//                                       @Query("format")String format);
 
-
-
-//?method=artist.getinfo&mbid=cc197bad-dc9c-440d-a5b5-d52ba2e14234&api_key=111&format=json
     @GET("?method=artist.getinfo&lang=es")Call<ArtistaRespuestaDetail> obtenerDetalleBanda(@Query("mbid")String mbid, @Query("api_key") String apikey,
                                                                                           @Query("format")String format);
-    //?method=artist.gettopalbums&mbid=cc197bad-dc9c-440d-a5b5-d52ba2e14234&api_key=123d7d5
     @GET("?method=artist.gettopalbums")Call<TopAlbum> obtenerAlbums(@Query("mbid")String mbid, @Query("api_key") String apikey,
                                                                     @Query("format")String format,@Query("page")int page);
-   // ?method=album.getinfo&api_key=123d7d50ffa67603998ec1042793fd68&mbid=219b202d-290e-3960-b626-bf852a63bc50&format=json
-
     @GET("?method=album.getinfo")Call<Albuminfo> obtenerDetallePista(@Query("api_key") String apikey,@Query("mbid")String mbid,@Query("format")String format);
 }
